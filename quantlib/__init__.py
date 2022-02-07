171f1e25519f90f5d2ef549e8bd882899bef15d8
brendamanganelli <72744528+brendamanganelli@users.noreply.github.com>
2022-02-07 19:10:06 +0100

    Update __init__.py

--- quantlib/__init__.py
+++ quantlib/__init__.py
@@ -1,6 +1,4 @@
 #i believe this file just needs to be created, for something something. 
 #and then, run pip install -e .
 
-#so this folder is where you will code your library
-
-#there is currently nothing inside the library yet
+#so this folder is where I will code your library
